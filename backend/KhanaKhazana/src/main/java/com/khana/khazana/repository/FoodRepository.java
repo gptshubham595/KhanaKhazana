b08c89f4b90967210c39f38fe9f2c106d98b12ad
gptshubham595 <gptshubham595@gmail.com>
2022-07-27 20:49:08 +0530

    food adding and checking already exist by restraunt_id and food_title

--- backend/KhanaKhazana/src/main/java/com/khana/khazana/repository/FoodRepository.java
+++ backend/KhanaKhazana/src/main/java/com/khana/khazana/repository/FoodRepository.java
@@ -2,12 +2,26 @@ package com.khana.khazana.repository;
 
 import com.khana.khazana.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
     Food findByFoodId(Long foodId);
+
+    boolean existsByFoodId(Long foodId);
+
+    @Query(value = "Select * from food where food_title=?1 and restaurant_id=?2", nativeQuery = true)
+    List<Food> findSameFoodTitleAndRestaurantId(String foodTitle, Long foodRestaurantId);
+
+    List<Food> findAllByFoodType(String foodType);
+
+
+    List<Food> findAllByRestaurantId(Long restaurantId);
+
 //    boolean find
 }
