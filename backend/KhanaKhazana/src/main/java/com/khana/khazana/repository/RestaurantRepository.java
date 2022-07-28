0832addd3f4429d288aa5e3517d7d9713dc8523f
apoorvlodhi-io <apoorvlodhi21@gmail.com>
2022-07-28 14:41:57 +0530

    restaurant-search-by-name

--- backend/KhanaKhazana/src/main/java/com/khana/khazana/repository/RestaurantRepository.java
+++ backend/KhanaKhazana/src/main/java/com/khana/khazana/repository/RestaurantRepository.java
@@ -1,5 +1,6 @@
 package com.khana.khazana.repository;
 
+import com.khana.khazana.model.Food;
 import com.khana.khazana.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,8 +8,8 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-    @Query(value = "Select * from product_details where pid=?1", nativeQuery = true)
-    Restaurant findRestaurantById(long pid);
+    @Query(value = "Select * from product_details where restaurant_id=?1", nativeQuery = true)
+    Restaurant findRestaurantById(long restaurant_id);
 
     Restaurant findByRestaurantId(long pid);
 
@@ -17,5 +18,16 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
     Restaurant findByManagerId(long managerId);
 
     List<Restaurant> findAll();
+
+    @Query(value="Select * from restaurant where restaurant_name=?1",nativeQuery = true)
+    Restaurant findRestaurantByName(String restaurant_name);
+
+    @Query(value="Select * from restaurant where restaurant_id = (select restaurant_id from food where food_name =?1)",nativeQuery = true)
+    List<Restaurant> findRestaurantByFood(String food);
+
+    @Query(value = "Select * from food", nativeQuery = true)
+    List<Food> findAllFoodItems();
+
+    Restaurant findByRestaurantId(Long restaurantId);
 
 }
