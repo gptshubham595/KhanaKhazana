f8e9bddf554c2a98bb5d7a6f9037ab92cf003cc0
apoorvlodhi-io <apoorvlodhi21@gmail.com>
2022-07-28 15:39:59 +0530

    response entity messages added for restaurant controller

--- backend/KhanaKhazana/src/main/java/com/khana/khazana/service/RestaurantService.java
+++ backend/KhanaKhazana/src/main/java/com/khana/khazana/service/RestaurantService.java
@@ -2,6 +2,7 @@ package com.khana.khazana.service;
 
 import com.khana.khazana.model.Food;
 import com.khana.khazana.model.Restaurant;
+import com.khana.khazana.model.RestaurantResponse;
 import com.khana.khazana.repository.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,12 +19,52 @@ public class RestaurantService {
         return restaurantRepository.findAll();
     }
 
-    public Restaurant getRestaurantsById(long restaurantId) {
-        return restaurantRepository.findRestaurantById(restaurantId);
+//    private long restaurantId;
+//    private String restaurantName;
+//    private String restaurantDescription;
+//    private Double restaurantRating;
+//    private String restaurantAddress;
+//    private String restaurantImage;
+//    private long managerId;
+//    private String message;
+
+    public RestaurantResponse getRestaurantsById(long restaurantId) {
+//        return r
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+        Restaurant restaurantFound = restaurantRepository.findRestaurantById(restaurantId);
+        if(restaurantFound == null){
+            restaurantResponse.setMessage("Sorry, the restaurant with id = " + restaurantId + " does not exist.");
+        }else{
+            restaurantResponse.setRestaurantId(restaurantFound.getRestaurantId());
+            restaurantResponse.setRestaurantName(restaurantFound.getRestaurantName());
+            restaurantResponse.setRestaurantDescription(restaurantFound.getRestaurantDescription());
+            restaurantResponse.setRestaurantRating(restaurantFound.getRestaurantRating());
+            restaurantResponse.setRestaurantAddress(restaurantFound.getRestaurantAddress());
+            restaurantResponse.setRestaurantImage(restaurantFound.getRestaurantImage());
+            restaurantResponse.setManagerId(restaurantFound.getManagerId());
+            restaurantResponse.setMessage("Success");
+        }
+        return restaurantResponse;
     }
 
-    public Restaurant getRestaurantByName(String name) {
-        return restaurantRepository.findRestaurantByName(name);
+    public RestaurantResponse getRestaurantByName(String name) {
+//        return restaurantRepository.findRestaurantByName(name);
+        RestaurantResponse restaurantResponse = new RestaurantResponse();
+        Restaurant restaurantFound = restaurantRepository.findRestaurantByName(name);
+        if(restaurantFound == null){
+            restaurantResponse.setMessage("Sorry, the restaurant " + name + " does not exist.");
+        }else{
+            restaurantResponse.setRestaurantId(restaurantFound.getRestaurantId());
+            restaurantResponse.setRestaurantName(restaurantFound.getRestaurantName());
+            restaurantResponse.setRestaurantDescription(restaurantFound.getRestaurantDescription());
+            restaurantResponse.setRestaurantRating(restaurantFound.getRestaurantRating());
+            restaurantResponse.setRestaurantAddress(restaurantFound.getRestaurantAddress());
+            restaurantResponse.setRestaurantImage(restaurantFound.getRestaurantImage());
+            restaurantResponse.setManagerId(restaurantFound.getManagerId());
+            restaurantResponse.setMessage("Success");
+        }
+        return restaurantResponse;
+
     }
     public List<Restaurant> getAllRestaurantsWithSpecificFood(String food) {
         return restaurantRepository.findRestaurantByFood(food);
